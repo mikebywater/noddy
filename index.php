1f4a14ccdbb75b727c6f5c5f287d32407cd1c003
mike.bywater <mike.j.bywater@gmail.com>
2015-04-10 17:39:37 +0100

    Added partials to views

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once 'vendor/autoload.php'; // Autoload files using Composer autoload
 require_once 'App/controllers/UsersController.php';
 require_once 'App/controllers/HomeController.php';
 
+
+
 $klein = new \Klein\Klein();
 
 
