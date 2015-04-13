9f5d1f6dfcb96602ee40cb41b0a7ec426d552825
Mike Bywater <mike.j.bywater@gmail.com>
2015-04-13 01:36:35 +0100

    Created artisan style generator
    Creates - models, views controllers and routes

--- config/database.php
+++ config/database.php
@@ -2,6 +2,6 @@
 
 define("DB_TYPE", "mysql");
 define("DB_HOST", "localhost");
-define("DB_NAME", "homestead");
+define("DB_NAME", "noddy");
 define("DB_USER", "homestead");
 define("DB_PASS", "secret");
